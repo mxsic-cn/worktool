6b3525e51e346cf64a93dfa5973f3376ed233eea
liuchuan <liuchuan@qq.com>
2019-07-04 10:26:11 +0800

    more fun

--- src/main/java/cn/siqishangshu/file/DownloadUtil.java
+++ src/main/java/cn/siqishangshu/file/DownloadUtil.java
@@ -1,5 +1,6 @@
 package cn.siqishangshu.file;
 
+import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
@@ -22,29 +23,28 @@ public class DownloadUtil {
             httpConnection = (HttpURLConnection) restServiceURL.openConnection();
             httpConnection.setRequestMethod("GET");
             if (httpConnection.getResponseCode() == 200) {
-                String fileName = UUID.randomUUID()+ ".pdf";
+                String fileName = UUID.randomUUID() + ".pdf";
                 FastDFSClientUtils fastDFSClientUtils = new FastDFSClientUtils();
                 InputStream inputStream = httpConnection.getInputStream();
-
-
-                byte[] bytes = new byte[inputStream.available()];
-                inputStream.read(bytes);
-
-//                File file = new File(fileName);
-//                System.out.println("local:"+file.getAbsolutePath());
-//                if (!file.exists()) {
-//                    file.createNewFile();
-//                    FileOutputStream outputStream = new FileOutputStream(file);
-//                    outputStream.write(bytes);
-//                    outputStream.close();
-//                }
-
-//                FileInputStream fileInputStream = new FileInputStream(file);
-//                bytes = new byte[fileInputStream.available()];
-//                fileInputStream.read(bytes);
-                String fileId = fastDFSClientUtils.uploadFile(bytes,fileName);
-                System.out.println("upload:"+fileId);
                 //随机生成文件名
+//                File file = File.createTempFile("ticket_", ".pdf");
+//                System.out.println("local:" + file.getAbsolutePath());
+//                FileOutputStream outputStream = new FileOutputStream(file);
+//                IOUtils.copy(inputStream ,outputStream );
+//                outputStream.flush();
+//                IOUtils.closeQuietly(outputStream);
+//                IOUtils.closeQuietly(inputStream);
+//                FileInputStream fileInputStream = new FileInputStream(file);
+//                byte[]  bytes = new byte[fileInputStream.available()];
+//                fileInputStream.read(bytes);
+//                inputStream.read(bytes);
+                ByteArrayOutputStream out = new ByteArrayOutputStream();
+                byte[] buffer = new byte[4096];
+                int readSize;
+                while ((readSize = inputStream.read(buffer)) >= 0) out.write(buffer, 0, readSize);
+                inputStream.close();
+                String fileId = fastDFSClientUtils.uploadFile(out.toByteArray(), fileName);
+                System.out.println("upload:" + fileId);
 
 
             }
